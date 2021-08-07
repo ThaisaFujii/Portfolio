2fcc244d7eeb143569357fe7cfdeecae3418f908
Luis Tavares <amanda_fujii@live.com>
2021-08-06 23:09:26 -0300

    conhecimento

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 // goal: me informa o estado do menu (exibido ou oculto)/container envolve todo mundo
+
